1432fda49c5d14fa9e133a74f0676f015a5c7c85
Edi <editos1212@gmail.com>
2024-01-18 10:52:21 +0100

    Competitions start

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { engine } = require("express-handlebars");
+const {parseAuthCookie} = require("./services/auth");
+
 
 // ROUTERS IMPORT
 const indexRouter = require('./routes/index');
@@ -24,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(parseAuthCookie);
 
 // ROUTERS SETUP
 app.use('/', indexRouter);
@@ -40,6 +43,10 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  if(err.status === 404){
+    res.locals.message = "Stranica ne postoji";
+  }
 
   // render the error page
   res.status(err.status || 500);
--- routes/competitions.js
+++ routes/competitions.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { checkAuthCookie } = require("../services/auth.js");
+const { authRequired } = require("../services/auth.js");
 
-router.get("/", checkAuthCookie, function (req, res, next) {
-    if(req.user)
-    {
-        console.log("Slobodan ulaz");
-    }
-    else{
-        console.log("Zabranjen ulaz");
-    }
+router.get("/", authRequired, function (req, res, next) {
     res.render("competitions/index");
   });
-
   module.exports = router;
